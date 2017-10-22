d68d5238a369851e8179b46d76fb3c5da6da335c
Ekaterina Morozova <31797019+kattiam@users.noreply.github.com>
2017-10-22 22:33:30 +0300

    Create HW6.cpp

---
+++ HW6.cpp
@@ -0,0 +1,26 @@
+//Написать программу, вычисляющую среднее арифметическое чисел в последовательности.
+//ДОМАШНЯЯ РАБОТА 22.10.2017
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int k, cnt, sum;
+	cin >> k;
+	cnt = 0;
+	sum = 0;
+  cnt = cnt + 1;
+  sum = sum + k;
+	}
+	while (k != 0) {
+		cin >> k;
+		cnt = cnt + 1;
+		sum = sum + k;
+	}
+	double l;
+	l = sum / cnt;
+	cout << l;
+	
+
+}
