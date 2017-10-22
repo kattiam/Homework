4ebacd9192b7baf965f1809f6484b142650e52f5
Ekaterina Morozova <31797019+kattiam@users.noreply.github.com>
2017-10-22 22:17:11 +0300

    Create HW5.cpp

---
+++ HW5.cpp
@@ -0,0 +1,17 @@
+//Вычислить произведение двух чисел, не используя операцию умножения.
+
+//ДОМАШНЯЯ РАБОТА 22.10
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int a, b, i;
+cin >> a >> b;
+for (i = 1; i < (b-1); i++) {
+a = a + b;
+}
+cout << a;
+return 0;
+}
