a4cb8dcf2539092a5893b66fb1a1ba75f70c89d8
Ekaterina Morozova <31797019+kattiam@users.noreply.github.com>
2017-11-12 19:26:57 +0300

    Update HW3.cpp

--- 12_11_2017/HW3.cpp
+++ 12_11_2017/HW3.cpp
@@ -7,7 +7,6 @@ Input:
 1A1        //3 строка
 2        //4 строка
 3  7        //5 строка
-
 Output:
 11: Prime
 22: Composite
@@ -17,8 +16,8 @@ Output:
 */
 
 /*
-Алгоритм: 
-Так как количество вводимых строк неизвестно, будем увеличивать 
+Алгоритм:
+Так как количество вводимых строк неизвестно, будем увеличивать
 начальный размер массива строк на addN (равное 5), когда текущий массив
 заполнен. Затем определяем тип каждого элемента массива.
 */
@@ -34,8 +33,10 @@ using namespace std;
 // Проверка на простоту числа a
 bool isPrime(long a){
     if (a == 2) return true;
+    if (a == -2) return true;
     if( (a==0) || (a%2 == 0) ) return false;
     if ( a == 1 ) return false;
+    if ( a == -1) return false;
     int k = a/2;   // sqrt(a) не используем, чтобы не подключать math lib
     for( long i = 3; i <= k; i++)
       if( a % i == 0) return false;
@@ -43,11 +44,13 @@ bool isPrime(long a){
 }
 // Возвращает тип введённой строки: 0 - Prime, 1 - Composite, 2 - Not a num
 int getStrType(string s) {
-    for (int i = 0; i < s.size(); i++) 
-      if( !(s[i] >= '0' && s[i] <= '9')) return 2;
+    for (int i = 0; i < s.size(); i++) {
+      if( s[0] == '-') continue;
+      if ( !(s[i] >= '0' && s[i] <= '9')) return 2;
+    }
     long k = atoi(s.c_str()); //String to Int
-    if( isPrime(k)) return 0; else return 1;  
-       
+    if( isPrime(k)) return 0; else return 1;
+
 }
 // Увеличиваем размер динамического массива
 //addN - на сколько увеличить
@@ -56,7 +59,7 @@ int getStrType(string s) {
 void incArrLen(int addN, int &N, string **arr) {
      string* newArr = new string [N + addN]; // выделяем новый массив
      string* p = *arr; // P - указатель на входной массив (адрес первого
-                       // элемента) 
+                       // элемента)
      for (int i = 0; i < N; i++) newArr[i] = p[i]; // заполняем новый массив
      delete [] p; // удаляем старый массив
      N = N + addN; // изменяем входное значение N
@@ -72,22 +75,22 @@ int main(int argc, char *argv[])
     int cnt = 0;
     string* arr = new string [N];
     cout << "Enter the strings." << endl;
-    cout << "The \"!\" is the end of the input." << endl; 
+    cout << "The \"!\" is the end of the input." << endl;
     while (true) {
       getline(cin, s);
       if( s[0] == '!') break;
       if( cnt >= N){ // массив заполнен полностью?
         incArrLen(addN, N, &arr); // увеличение размера массива
-      }  
+      }
       arr[cnt] = s;
-      cnt++;        
+      cnt++;
     }
     for( int i = 0; i < cnt; i++) {
       int ind = getStrType(arr[i]);
-      cout << arr[i] << ":" << " " << strType[ind] << endl; 
+      cout << arr[i] << ":" << " " << strType[ind] << endl;
     }
-    
-    
+
+
     system("PAUSE");
     return EXIT_SUCCESS;
 }
